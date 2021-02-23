9642cad564344daaf31a737586513a72a41ef243
Derrick <derrick.chow234@gmail.com>
2021-02-23 13:17:56 -0500

    Adeed LA time and millis

--- app/components/simple-date-time.js
+++ app/components/simple-date-time.js
@@ -4,10 +4,28 @@ import { tracked } from "@glimmer/tracking";
 import { later } from "@ember/runloop";
 
 export default class SimpleDateTimeComponent extends Component {
-  @tracked date = new Date().toLocaleTimeString();
+  @tracked date = new Date().toLocaleDateString();
+  @tracked time = new Date().toLocaleTimeString();
+  @tracked milli = new Date().getMilliseconds();
+
+  @tracked oDate = new Date().toLocaleDateString("en-US", {
+    timeZone: "America/Los_Angeles",
+  });
+  @tracked oTime = new Date().toLocaleTimeString("en-US", {
+    timeZone: "America/Los_Angeles",
+  });
 
   @action
-  getDate() {
-    this.date = new Date().toLocaleTimeString();
+  getDateAndTime() {
+    this.date = new Date().toLocaleDateString();
+    this.time = new Date().toLocaleTimeString();
+    this.milli = new Date().getMilliseconds();
+
+    this.oDate = new Date().toLocaleDateString("en-US", {
+      timeZone: "America/Los_Angeles",
+    });
+    this.oTime = new Date().toLocaleTimeString("en-US", {
+      timeZone: "America/Los_Angeles",
+    });
   }
 }
